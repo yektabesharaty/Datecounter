d1571e23fc019bf0637c173eafbf6db238172552
yekta <yektabesharaty@gmail.com>
2024-08-10 17:17:02 +0330

    Update Project

--- src/App.js
+++ src/App.js
@@ -21,17 +21,37 @@ export default function App() {
   function handlecount2() {
     setcount(count + step);
   }
+  function handleReset() {
+    setstep(1);
+    setcount(0);
+  }
 
   return (
     <div className="App">
       <div className="button">
-        <button onClick={handlestep1}>-</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          //با هربار تغیر عدد توی اون رنجه مقدارشو میگیره و تغییر میکنه
+
+          onChange={(e) => setstep(Number(e.target.value))}
+        />
+
+        {/* <button onClick={handlestep1}>-</button> */}
         <p>Step :{step}</p>
-        <button onClick={handlestep2}>+</button>
+        {/* <button onClick={handlestep2}>+</button> */}
       </div>
       <div className="button">
         <button onClick={handlecount1}>-</button>
-        <p>Count: {count}</p>
+        {/* <p>Count: {count}</p> */}
+
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setcount(Number(e.target.value))}
+        />
         <button onClick={handlecount2}>+</button>
       </div>
       <p>
@@ -43,6 +63,12 @@ export default function App() {
             : `${Math.abs(count)} days ago was ${date.toDateString()} `}
         </span>
       </p>
+      {/* وقتی میخایم از یا اند استفاده کنیم باید از شرطی کردن ؟ استفاده کنیم نه && */}
+      {step !== 1 || count !== 0 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
